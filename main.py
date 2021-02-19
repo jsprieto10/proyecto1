3e4b21ed9f4b64b9920a846c4d7aadcfeb6bb29e
Sara María Bejarano <sm.bejarano44@uniandes.edu.co>
2021-02-19 15:26:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hola mundo y yirleny y anyi!'
+    return '<p>Hola mundo, Yirleny y Anyi! estas son las rutas que puedes explorar: <br/>\
+<ul>\
+<li>/cambio</li> \
+<li>/saludar</li>\
+<li>/saludar/ACA_PONES_TU_NOMBRE</li>\
+</ul></p>'
 
 @app.route('/cambio')
 def git():
@@ -11,6 +16,10 @@ def git():
 
 @app.route('/saludar')
 def saludar():
-    return 'Hola chicos, desatrasandome estoy!'
+    return 'Hola chicos, desatrasandome estoy! <b>ahora entra a la ruta 127.0.0.1:500/saludar/TUNOMBRE</b>'
+
+@app.route('/saludar/<nombre>')
+def saludar_nombre(nombre):
+    return f'<h2>Hola {nombre}!</h2> <p>Ya pudiste bajar mis cambios en el repo :)</p>'
     
 app.run()
