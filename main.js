19534edafc3576501bb2cebe053dd035dc51bb54
Anyi Reyes <77987016+AnyiYuVaRe@users.noreply.github.com>
2021-06-15 13:39:42 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,34 +1,80 @@
+//alert('Hola Sebas, Sara y Yirlenys!!!');
+var nombre = "Anyi Reyes";
+var años   = 22;
+
+/*
+var concatenacion = nombre + " " + años;
+
+var datos = document.getElementById("datos");
+datos.innerHTML = `
+    <h1>Soy la caja de datos</h1>
+    <h2>Mi nombre es: ${nombre}</h2>
+    <h3>Tengo: ${años} años</h3>
+`;
+
+if(años >= 22){
+    datos.innerHTML += '<h1>Eres joven aun</h1>';
+}else{
+    datos.innerHTML += '<h1>Ya estas muy joven</h1>';
+}
+
+for (var i = 2000; i<=2021; i++){
+    //bloque de instrucciones
+    datos.innerHTML += "<h2>Estamos en el año: " + i;
+}
+*/
+/*
+function MuestraMiNombre(nombre, años){
+    var datos = document.getElementById("datos");
+    datos.innerHTML = `
+    <h1>Soy la caja de datos</h1>
+    <h2>Mi nombre es: ${nombre}</h2>
+    <h3>Tengo: ${años} años</h3>
+    `;
+}
+
+MuestraMiNombre("Anyi Reyes WEB", 22);
+*/
+
+function MuestraMiNombre(nombre, años){
+    var misDatos = `
+    <h1>Soy la caja de datos</h1>
+    <h2>Mi nombre es: ${nombre}</h2>
+    <h3>Tengo: ${años} años</h3>
+    `;
+
+    return misDatos;
+}
+
+function imprimir (){
+    var datos = document.getElementById("datos");
+    datos.innerHTML = MuestraMiNombre("Anyi Reyes", 22);
+}
+
+imprimir();
+
+var nombres = ['Alejandra', 'Angélica', 'Zoe'];
+//alert(nombres[1]);
+
+document.write('<h1>Listado de nombres</h1>');
+/*
+for(i = 0; i < nombres.length; i++){
+    document.write(nombres[i] + '</br/>');
+}
+*/
+nombres.forEach((nombre) => {
+    document.write(nombre + '<br/>');
+});
+//var datos = document.getElementById("datos");
+//datos.innerHTML = concatenacion;
+
+//document.write(concatenacion);
+
+//document.write(nombre);
+//document.write(años);
 
 
-var btnCrear = document.getElementById("btn-create")
-
-btnCrear.onclick = function(e){
-    e.preventDefault();
-
-    var inputNombre = document.getElementById("nom")
-    var inputCali = document.getElementById("cali")
-    var inputDuracion = document.getElementById("duracion")
-    var inputAnio = document.getElementById("anio")
 
 
 
 
-    var raw = JSON.stringify({"nombre":inputNombre.value,"duracion":inputDuracion.value,"calificacion":inputCali.value,"año":inputAnio.value});
-
-    console.log(raw)
-    var requestOptions = {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-      },
-    body: raw,
-    mode: 'cors'
-    };
-
-    fetch("http://127.0.0.1:5000/pelicula", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
-
-
-    }
