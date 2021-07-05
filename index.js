eb290641e9e65b1a7941dfdff2c2a716e402e94e
yirlenyFaithful <yirleny2193@hotmail.com>
2021-07-04 20:31:55 -0500

    clases practicas

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 // asigarnle una funcion al boton crear pelicula
-
-
+// 1. ubijcarme en el boton
+var btnCreate = document.getElementById("btn-create");
+// escribir la función
+btnCreate.onclick = function(e){
+e.preventDefault();
 // obtener los valores de los campos
-
+var nombre = document.getElementById("nom").value;
+var calificacion = document.getElementById("cali").value;
+var duracion = document.getElementById("duracion").value;
+var anio = document.getElementById("anio").value;
 
 // hacer petición
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
 
+var raw = JSON.stringify({"nombre":nombre,"duracion":duracion,"calificacion":calificacion,"anio":anio});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("http://127.0.0.1:5000/pelicula", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+}
+
+//opciones para hacer purva 
+// alert.
+//log.
--- main.py
+++ main.py
@@ -6,15 +6,45 @@ app = Flask(__name__)
 CORS(app)
 
 
+#SQlite 
+
 @app.route('/pelicula', methods=['POST'])
 def crear_pelicula():
+    #Obtener la información de una petición
+    inf = request.get_json()
+    nombre = inf["nombre"]
+    duracion = inf["duracion"]
+    calificacion = inf ["calificacion"]
+    anio =inf["anio"]
+
+    #conectarnos a la bd
+    con = sqlite3.connect('peliculas.db')
+    #insertar en la base de db
+    cursorObj = con.cursor()
+    entities = (nombre, calificacion, duracion, anio)
+    cursorObj.execute('INSERT INTO pelicula(nombre, calificacion, duracion, año) VALUES(?, ?, ?, ?)', entities)
+
+    con.commit()
+
+
     # completar la función
-    return
+    return {"res":"okey"}
 
 @app.route('/pelicula/<nombre>', methods=['DELETE'])
 def borrar_pelicula(nombre):
+    #completar la función
+    #conectarnos a la bd
+    con = sqlite3.connect('peliculas.db')
+    #insertar en la base de db
+    cursorObj = con.cursor()
+    print(nombre)
+    cursorObj.execute('DELETE FROM pelicula where nombre= ?', [nombre])
+
+    con.commit()
+
+
     # completar la función
-    return
+    return {"res":"okey"}
 
 
 app.run(debug=True)
